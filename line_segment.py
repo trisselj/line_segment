fa7fab855b92b8786b3bd5a44d5816e33449775b
trisselj <trisselj@oregonstate.edu>
2024-08-12 12:25:46 -0700

    q

--- line_segment.py
+++ line_segment.py
@@ -3,7 +3,7 @@
 # Date: 08/07/2024
 # Description: Defines a Point class for representing 2D coordinates and calculating the distances between points, along with a LineSegment class that uses the points to compute the length, slope, and parallelism of the line segments.
 
-class point: # Initializing the class
+class Point: # Initializing the class
     def __init__(self, x_coord, y_coord): #INitializing the private x and y coords
         self._x_coord = x_coord
         self._y_coord = y_coord
@@ -17,7 +17,7 @@ class point: # Initializing the class
     def distance_to(self, other_point): # Calculates and returns the distance to another point object
         return ((other_point.get_x_coord() - self._x_coord) ** 2 + (other_point.get_y_coord() - self._y_coord) ** 2) ** 0.5
     
-class LineSegment: # Initializers the class
+class LineSegment: # Initializes the class
     def __init__(self, endpoint_1, endpoint_2): # Initializes the private endpoints of the line segment
         self._endpoint_1 = endpoint_1
         self._endpoint_2 = endpoint_2
